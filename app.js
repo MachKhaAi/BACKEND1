508648b0d91b7735cfa76e8fb9d7606ba4494c94
MachKhaAi_B2303797_Backend1 <aib2303797@student.ctu.edu.vn>
2025-11-01 10:21:02 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const contactsRouter = require("./app/routes/contact.route");
 const express = require("express"); 
 const cors = require("cors");
-
+const ApiError = require("./app/api-error");
 const app = express(); 
 
 app.use(cors()); 
@@ -11,5 +11,13 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." }); 
 });
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found")); 
+});
+app.use((err, req, res, next) => { 
+  return res.status(err.statusCode || 500).json({ 
+    message: err.message || "Internal Server Error", 
+  });
+});
 
 module.exports = app; 
